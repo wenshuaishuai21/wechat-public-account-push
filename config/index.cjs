3e262c3250dc71ee3ba9ae25cf62a8ae3d0063d1
wenshuaishuai21 <119398372+wenshuaishuai21@users.noreply.github.com>
2022-11-29 16:32:57 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oVMXa6aY0_asPYUWz0gHzfX827es',
+      id: 'oVMXa6T9zxFoXibVPBM2jgLGaP8Q',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '5tLHJG4zGKe-wxmA_lJwHn1fZHmb0g5Ri8bUO3Wh4oE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '5tLHJG4zGKe-wxmA_lJwHn1fZHmb0g5Ri8bUO3Wh4oE',
+  CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oVMXa6T9zxFoXibVPBM2jgLGaP8Q',
+      id: '',
     }
   ],
 
